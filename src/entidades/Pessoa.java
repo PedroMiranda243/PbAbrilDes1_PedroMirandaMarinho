d0d7bec673c880aec6f85b1ed5484c34305e02e9
Pedro Miranda <pedromarinho243@hotmail.com>
2024-05-01 22:38:43 -0300

    arvore correta sem formatação

--- src/entidades/Pessoa.java
+++ src/entidades/Pessoa.java
@@ -1,9 +1,45 @@
 package entidades;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Pessoa {
     private String name;
     private Pessoa conjuge;
-    private Pessoa filho;
+    private ArrayList <Pessoa> filho = new ArrayList<>();
+
+    public Pessoa(String name) {
+        this.name = name;
+    }
+
+    public Pessoa(String name, Pessoa pai) {
+        this.name = name;
+        pai.adcFilho(this);
+    }
+
+    public void adcConjuge(Pessoa p){
+        conjuge = p;
+    }
+    
+    public void adcFilho(Pessoa p){
+        filho.add(p);
+    }
+
+    public void arvore(int x){
+        if (filho == null){//caso base
+       return;
+      }
+      String format = "";
+      for (int i = 0; i < x; i++) {
+        format += " ";
+      }
+      String construtor = format + name +  ((conjuge == null) ? " -- Solteiro" : " -- Casado com: " + conjuge.name) + ((filho.isEmpty()) ? "" :" -- Filhos:"); 
+      System.out.println(construtor);
+      for (Pessoa c: filho) {
+        c.arvore(x++);
+      } 
+    }
 
     
+
 }
