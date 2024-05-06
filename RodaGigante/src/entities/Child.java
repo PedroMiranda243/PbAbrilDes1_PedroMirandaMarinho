58f26881e3db73fa4f00773a1938bc9510a728ee
Pedro Miranda <142632758+PedroMiranda243@users.noreply.github.com>
2024-05-06 10:52:49 -0300

    Implementação da classe Child e herança

--- RodaGigante/src/entities/Child.java
+++ RodaGigante/src/entities/Child.java
@@ -1,5 +1,25 @@
 package entities;
 
-public class Child {
+public class Child extends Person {
+    private int age;
+    private Adult father;
 
+    public Child(String name, int age, Adult father) {
+        super(name);
+        this.age = age;
+        this.father = father;
+    }
+
+    public Child(String name, int age) {
+        super(name);
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Adult getFather() {
+        return father;
+    }
 }
