45b814633b71b59eb0daa0860de1b53f1443d152
Pedro Miranda <142632758+PedroMiranda243@users.noreply.github.com>
2024-05-06 10:56:06 -0300

    Implementa ção da classe FerrisWheel

--- RodaGigante/src/entities/FerrisWheel.java
+++ RodaGigante/src/entities/FerrisWheel.java
@@ -1,5 +1,46 @@
 package entities;
 
-public class FerrisWheel {
+import java.util.HashMap;
+import java.util.Map;
 
+public class FerrisWheel {
+  private Map<Integer, String> gondolaStatus;
+
+    public FerrisWheel() { //Inicializa gondolaStatus com 18(chaves) godolas vazias(valor)
+        gondolaStatus = new HashMap<>();
+        for (int i = 1; i <= 18; i++) {
+            gondolaStatus.put(i, "(vazia)");
+        }
+    }
+
+    // Método para embarcar passageiros na roda-gigante
+    public void board(int gondolaNumber, Person... people) {
+        boolean underTwelve = false;
+        boolean fatherPresent = false;
+        for (Person person : people) {
+            if (person instanceof Child && ((Child) person).getAge() < 12) {
+                underTwelve = true;
+                if (((Child) person).getFather() != null) {
+                    fatherPresent = true;
+                }
+            }
+        }
+
+        if (underTwelve && !fatherPresent) {
+            System.out.println("ERRO: Criança menor de 12 anos sem o pai presente!");
+        } else {
+            StringBuilder passengers = new StringBuilder();
+            for (Person person : people) {
+                passengers.append(person.getName()).append(" ");
+            }
+            gondolaStatus.put(gondolaNumber, passengers.toString());
+        }
+    } 
+     public void status() {
+        System.out.println("Status das Gôndolas");
+        System.out.println("-----------------------------");
+        for (Map.Entry<Integer, String> entry : gondolaStatus.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
 }
